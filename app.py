bd18b1edfd99dba41cd4de9a8ca90dae97888cae
Harika Munjeti <harikamunjeti915@gmail.com>
2026-06-27 18:57:25 +0530

    Updated chatbot code

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 from flask import Flask, render_template, request, session
 from chatbot import get_answer
-
-
 app = Flask(__name__)
-
 app.secret_key = "chatbot_secret_key"
-
-
-
 @app.route("/", methods=["GET", "POST"])
 def home():
-
     if "messages" not in session:
         session["messages"] = [
             {
@@ -18,53 +11,29 @@ def home():
                 "text": "Hi 👋 I am your Laptop Assistant. How can I help you?"
             }
         ]
-
-
     if request.method == "POST":
-
         question = request.form["question"]
-
         answer = get_answer(question)
-
-
         session["messages"].append(
             {
                 "type": "user",
                 "text": question
             }
         )
-
-
         session["messages"].append(
             {
                 "type": "bot",
                 "text": answer
             }
         )
-
-
         session.modified = True
-
-
-
     return render_template(
         "index.html",
         messages=session["messages"]
     )
-
-
-
-
 @app.route("/clear")
 def clear():
-
     session.clear()
-
     return home()
-
-
-
-
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
